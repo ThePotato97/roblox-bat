d1a743655484947212136e99253abd9781d3d656
ThePotato97 <10260415+ThePotato97@users.noreply.github.com>
2023-09-24 04:54:57 +0100

    generate docs

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslLycHJ0zslMzStR0lEqSCzJAAol5yQWF6cW68Ol9DJKcnOA8tmZeSlKVoZGFrU6cP1uriHOHvEh/t6ufvG+riGOLo4hjvHBrj6uziH+QQgzyxKLMhOTcoCm4tWAapOxEZJFEB0ero4urkHxfo6+rtgMx1CEYWAsAFZZkAz7AAAA"
